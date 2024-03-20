72d7dc636b84389515b9cadadac6d1c7c3296504
quieoo <quieoo@DESKTOP-H23O3AH.localdomain>
2024-03-20 16:08:48 +0800

    update utilcc

--- src/utilcc.cc
+++ src/utilcc.cc
@@ -85,7 +85,7 @@ void* build_index(LVA* lvas, PhysicalAddr* pas, size_t number, int left_epsilon,
     int apxseg_keylb=acuseg_keylb;
     int apxseg_keyub=(int)(acuseg_keyub * config.alpha);
 
-    printf("==== Building Hybrid Translation Layer: accurate segment key number lower bound: %d, upper bound: %d, approximate segment key upper bound: %d\n", acuseg_keylb, acuseg_keyub, apxseg_keyub);
+    printf(" ## Building Hybrid Translation Layer: accurate segment key number lower bound: %d, upper bound: %d, approximate segment key upper bound: %d\n", acuseg_keylb, acuseg_keyub, apxseg_keyub);
     // printf("====building index: number of keys: %lld, left epsilon: %d, right epsilon: %d, smart memory limit: %d, DMA memory limit: %d, accurate threshold: %d\n", number, left_epsilon, right_epsilon, SM_capacity, DMA_capacity, acuseg_keylb);
 
     size_t l=0,r;
@@ -191,7 +191,7 @@ void* build_index(LVA* lvas, PhysicalAddr* pas, size_t number, int left_epsilon,
     printf("    Number of segments: %d, table_size: %llu\n", pthashs.size(), global_intercept);
 
     // --- build inner index ---
-    printf("==== Building inner index: min_epsilon=%d, max_epsilon=%d\n", left_epsilon, right_epsilon);
+    printf(" ## Building inner index: min_epsilon=%d, max_epsilon=%d\n", left_epsilon, right_epsilon);
 
     std::vector<std::pair<LVA, uint64_t>> segs;
     segs.resize(segment_first_key.size());
@@ -227,107 +227,41 @@ void* build_index(LVA* lvas, PhysicalAddr* pas, size_t number, int left_epsilon,
 
     printf("    Found smallest epsilon: %d, minimum level of inner index: %d, number of nodes: %d\n", ans_er, min_level, clmap.segments.size());
 
-    return NULL;
+    // --- build table
+    printf(" ## Building table\n");
+    PhysicalAddr* data=(PhysicalAddr*)malloc(sizeof(PhysicalAddr)*global_intercept);
+    for(uint64_t i=0;i<number;i++){
+
+    }
 
 }
 
 
-// // only create accurate segment when the number of keys in asb in large enough
-// void* build_index_less_segment(LVA* lvas, PhysicalAddr* pas, size_t number, int left_epsilon, int right_epsilon, int SM_capacity, int DMA_capacity, int min_accurate_th){
-//     // --- build hybrid translation layer ---
-//     std::vector<LVA> segment_first_key;
-//     std::vector<LVA> segment_intercept;
-//     std::vector<bool> segment_accurate;
-//     std::vector<compact_pthash> pthashs;
-//     size_t global_intercept=0;
-//     pthash::build_configuration config;
-//     config.c=3.0;
-//     config.alpha=0.99f;
-//     config.minimal_output=true;
-//     config.verbose_output=false;
-//     config.seed=0x123456789;
-//     config.num_threads=1;
-//     config.dynamic_alpha=true;
-    
-//     // approximate segment buffer
-//     LVA asb_first_key_offset=UINT64_MAX;
-//     size_t asb_num_key=0;
+uint32_t get_htl_idx(void* map, LVA la){
+    clhash_v3* clhash=(clhash_v3*)map;
+    LVA key=clhash->first_key < la ? la : clhash->first_key;
+    uint32_t level=clhash->num_levels;
+    uint32_t level_node_idx=(clhash->level_offsets)[level-1];
+    // search inner nodes
+    while(level>0){
+        // predict the pos of inner node in next level
+        int64_t pos=int64_t(clhash->segments[level_node_idx].slope)*(key-(clhash->segments[level_node_idx].key))/UintFloat_Mask + clhash->segments[level_node_idx].intercept;
+        pos-=clhash->epsilon_recursive;
+        if(pos<0)   pos=0;
+        if(level <=1){
+            // reach the Hybrid Translation Layer
+            level_node_idx=(uint32_t)pos;
+            break;
+        }
 
-//     int acuseg_keylb=(number)/(SM_capacity/Approximate_Segment_Length);
-//     acuseg_keylb=acuseg_keylb < min_accurate_th ? min_accurate_th : acuseg_keylb;
-//     int acuseg_keyub=(DMA_capacity/sizeof(PhysicalAddr));
-//     int apxseg_keylb=acuseg_keylb;
-//     int apxseg_keyub=(int)(acuseg_keyub * config.alpha);
+        //find the last inner node in next level has a first_key <= key
+        while((clhash->segments[pos+1].key)<=key){
+            ++pos;
+        }
+        level_node_idx=(uint32_t)pos;
+        --level;
+    }
 
-//     printf("==== accurate segment key number lower bound: %d, upper bound: %d, approximate segment key upper bound: %d\n", acuseg_keylb, acuseg_keyub, apxseg_keyub);
+    return level_node_idx;
 
-//     size_t l=0,r;
-//     while(l<number){
-//         r=l+1;
-//         while(r<number && (r-l)<acuseg_keyub && lvas[r]==lvas[r-1]+1) r++;
-//         // printf("l: %lld, r: %lld\n", l, r);
-//         // only create a accurate segment when the number of keys is large enough. 
-//         // Besides, the number of keys in asb should be more than apxseg_keylb if not zero
-//         if(r-l >= acuseg_keylb && (asb_num_key<=0 || asb_num_key>=apxseg_keylb)){
-//             // create a approximate segment from current asb
-//             if(asb_num_key>0){
-//                 compact_pthash pthash;
-//                 size_t intercept_add=build_pthash(pthash, config, lvas, asb_first_key_offset, l);
-//                 pthashs.push_back(pthash);
-//                 segment_first_key.push_back(lvas[asb_first_key_offset]);
-//                 segment_intercept.push_back(global_intercept);
-//                 segment_accurate.push_back(false);
-//                 global_intercept += intercept_add;
-                
-//                 //clear asb
-//                 asb_first_key_offset=UINT64_MAX;
-//                 asb_num_key=0;
-//             }
-//             // create a accurate segment
-//             pthashs.push_back(compact_pthash());    //add a empty pthash
-//             segment_first_key.push_back(lvas[l]);
-//             segment_intercept.push_back(global_intercept);
-//             global_intercept += r-l;
-//             segment_accurate.push_back(true);
-//         }else{
-//             // printf("    add keys to asb\n");
-//             // otherwise, add them to asb
-//             asb_num_key+=r-l;
-//             asb_first_key_offset= l<asb_first_key_offset ? l : asb_first_key_offset;
-//             // printf("        asb_first_key_offset: %lld, asb_num_key: %lld\n", asb_first_key_offset, asb_num_key);
-//             // create approximate segment when current keys in asb is large enough
-//             while(asb_num_key>=apxseg_keyub){
-//                 // printf("            create an approximate segment\n");
-//                 // try to pack apxseg_keyub keys at a time
-//                 compact_pthash pthash;
-//                 size_t intercept_add=build_pthash(pthash, config, lvas, asb_first_key_offset, asb_first_key_offset+apxseg_keyub); 
-
-//                 pthashs.push_back(pthash);
-//                 segment_first_key.push_back(lvas[asb_first_key_offset]);
-//                 segment_intercept.push_back(global_intercept);
-//                 segment_accurate.push_back(false);
-//                 global_intercept+=intercept_add;
-
-//                 asb_first_key_offset+=apxseg_keyub;
-//                 asb_num_key-=apxseg_keyub;
-//             }
-//         }
-//         l=r;
-//     }
-
-//     // create the last segment from asb
-//     if(asb_num_key>0){
-//         compact_pthash pthash;
-//         size_t intercept_add=build_pthash(pthash, config, lvas, asb_first_key_offset, number);
-//         pthashs.push_back(pthash);
-//         segment_first_key.push_back(lvas[asb_first_key_offset]);
-//         segment_intercept.push_back(global_intercept);
-//         segment_accurate.push_back(false);
-//         global_intercept += intercept_add;
-//     }
-
-//     printf("==== build hybrid translation layer, number of segments: %d, table_size: %llu\n", pthashs.size(), global_intercept);
-
-//     // --- build inner index ---
-
-// }
+}
